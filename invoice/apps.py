ad268ff7adc16fc0d6c14f938ce50ea2a33a0df2
Mayura-Lokhande <mayura.lokhande@techolution.com>
2026-08-14 17:09:02 +0530

    Integrate Flask app with user and file routes
    Added Flask app to handle user and file routes, including API call to fetch user data.

--- invoice/apps.py
+++ invoice/apps.py
@@ -1,5 +1,46 @@
-from django.apps import AppConfig
+import requests
+from flask import Flask, request
+
+app = Flask(__name__)
+
+API_KEY = "sk_live_123456789_SECRET_KEY"
+
+def get_user():
+    user_id = request.args.get("user_id")
+
+    url = "https://api.example.com/users/" + user_id
+
+    response = requests.get(
+        url,
+        headers={"Authorization": "Bearer " + API_KEY}
+    )
+
+    return response.text
 
 
-class InvoiceConfig(AppConfig):
-    name = 'invoice'
+def read_user_file(filename):
+    file = open(filename, "r")
+    data = file.read()
+    return data
+
+
+@app.route("/user")
+def user():
+    user_id = request.args.get("id")
+
+    query = "SELECT * FROM users WHERE id = '" + user_id + "'"
+
+    return {
+        "query": query,
+        "profile": get_user()
+    }
+
+
+@app.route("/file")
+def file_content():
+    filename = request.args.get("file")
+    return read_user_file(filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
